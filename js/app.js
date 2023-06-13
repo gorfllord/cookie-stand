7e4346b99b7f1df377883e1f152e091867d03097
Geoff Lloyd <orionblack@gmail.com>
2023-06-13 15:43:50 -0700

    Finished lab7 code, initializing css reset

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const pageContainer = document.getElementById('salesNumbers');
-
+let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+let allCities = [];
+let table = document.createElement('table');
+let tbody = document.createElement('tbody');
 function totalCookies(totalArr) {
   let sum = 0;
   for (let i = 0; i < totalArr.length; i++) {
@@ -9,6 +12,54 @@ function totalCookies(totalArr) {
   }
   return sum;
 }
+function renderTableHeader() {
+  let tr = document.createElement('tr');
+  pageContainer.appendChild(table);
+  table.appendChild(tbody);
+  tbody.appendChild(tr);
+  let topLeft = document.createElement('th');
+  topLeft.textContent = 'City';
+  tr.appendChild(topLeft);
+  for (let i = 0; i < hours.length; i++){
+    let x = hours[i];
+    let header = document.createElement('th');
+    header.textContent = x;
+    tr.appendChild(header);
+  }
+}
+function renderTableData(city) {
+  let newRow = document.createElement('tr');
+  tbody.appendChild(newRow);
+  let x = city;
+  let cityName = document.createElement('th');
+  cityName.textContent = x.name;
+  newRow.appendChild(cityName);
+  x.getRandom();
+  for (let j = 0; j < x.sales.length; j++) {
+    let y = `${x.sales[j]}`;
+    let data = document.createElement('td');
+    data.textContent = y;
+    newRow.appendChild(data);
+  }
+}
+function renderTotalRow(cityArr) {
+  let totalRow = document.createElement('tr');
+  tbody.appendChild(totalRow);
+  let total = document.createElement('th');
+  total.textContent = 'Total:';
+  totalRow.appendChild(total);
+  for (let i = 0; i < hours.length; i++) {
+    let hourlyTotals = [];
+    for (let j = 0; j < cityArr.length; j++) {
+      hourlyTotals.push(cityArr[j].sales[i]);
+    }
+    let totalHour = totalCookies(hourlyTotals);
+    let totalData = document.createElement('td');
+    totalData.textContent = totalHour;
+    totalRow.appendChild(totalData);
+  }
+}
+
 function City(name, min, max, avg) {
   this.name = name,
   this.min = min,
@@ -17,23 +68,25 @@ function City(name, min, max, avg) {
   this.hourlySales = function() {
     return Math.floor((Math.random() * (this.max - this.min + 1) + this.min) * this.avg);
   },
-  this.hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
   this.sales = [],
-  this.logBreakdown = function(ul) {
-    for (let i = 0; i < this.hours.length; i++) {
+  this.getRandom = function() {
+    for (let i = 0; i < hours.length; i++) {
       let x = this.hourlySales();
       this.sales.push(x);
     }
-    for (let i = 0; i < this.hours.length; i++) {
+  },
+  this.logBreakdown = function(ul) {
+    this.getRandom();
+    for (let i = 0; i < hours.length; i++) {
       let listItem = document.createElement('li');
-      listItem.textContent = `${this.hours[i]}: ${this.sales[i]} cookies`;
+      listItem.textContent = `${hours[i]}: ${this.sales[i]} cookies`;
       ul.appendChild(listItem);
     }
     let total = document.createElement('li');
     total.textContent = `Total: ${totalCookies(this.sales)} cookies`;
     ul.appendChild(total);
   },
-  this.render = function() {
+  this.renderList = function() {
     let article = document.createElement('article');
     let ul = document.createElement('ul');
     let h3 = document.createElement('h3');
@@ -43,6 +96,7 @@ function City(name, min, max, avg) {
     article.appendChild(h3);
     article.appendChild(ul);
   };
+  allCities.push(this);
 }
 
 let seattle = new City('Seattle', 23, 65, 6.3);
@@ -51,8 +105,16 @@ let dubai = new City('Dubai', 11, 38, 3.7);
 let paris = new City('Paris', 20, 38, 2.3);
 let lima = new City('Lima', 2, 16, 4.6);
 
-seattle.render();
-tokyo.render();
-dubai.render();
-paris.render();
-lima.render();
+renderTableHeader();
+renderTableData(seattle);
+renderTableData(tokyo);
+renderTableData(dubai);
+renderTableData(paris);
+renderTableData(lima);
+renderTotalRow(allCities);
+
+// seattle.render();
+// tokyo.render();
+// dubai.render();
+// paris.render();
+// lima.render();
